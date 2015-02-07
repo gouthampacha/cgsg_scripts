8ca9caf6a6fccf249b2f0dce0e24268d93db2ea9
Goutham (Laptop) <gpachar@g.clemson.edu>
2015-02-07 02:59:48 -0500

    Final mods to match PHP 5.3 specs

--- gsg_news_feed.php
+++ gsg_news_feed.php
@@ -1,31 +1,57 @@
 <?php
     
+    $feed_url = 'http://blogs.clemson.edu/graduatestudentgovernment/feed/';
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://blogs.clemson.edu/graduatestudentgovernment/feed/');
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $feed_url);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $data = curl_exec($ch);
     curl_close($ch);
 
-    $xml = new SimpleXMLElement($data, LIBXML_NOCDATA);
+    
+    echo "<!DOCTYPE html> <html><head><title></title></head><body>";
+    
 
-    echo "<ul>";
+    $xml = produce_XML_object_tree($data);
+
      
     foreach($xml->channel->item as $entry) {
-        $desc = simplexml_load_string($entry->description);
-        echo "<li>";
-        if($desc->img) {
+            $desc = simplexml_load_string($entry->description, null, LIBXML_NOCDATA);
+       
+           echo "<ul>";
+            echo "<li>";
+                echo "<a href='".$entry->link."' title='"."$entry->title'>";
+                if(!empty($desc->img)) {
 
-            echo "<img src=' ". $desc->img['src'][0]. "' height='200px'>";
-        } else {
-            echo "<img src='http://www.clemson.edu/students/cgsg/global/images/GSGLogo-Orange.jpeg' height='200px'>"; 
-        }
-        echo "<br /><a href='$entry->link' title='$entry->title'>" . $entry->title . "</a></li>";
+                    echo "<img alt='' src=' ". $desc->img['src'][0]. "' height='187px' width='340px'>";
+                } else {
+                    echo "<img alt='' src='http://www.clemson.edu/students/cgsg/global/images/GSGLogo-Orange.jpeg' height='187px' width='340px'>"; 
+                }
+            echo "</li>";
+           echo "</ul>";
+           echo "<p class='last'>" . $entry->title . "</p>";
         
         
     }
-    echo "</ul>";
+
+
+
+    //Function to produce XML tree without dumping errors to the page
+    function produce_XML_object_tree($raw_XML) {
+    libxml_use_internal_errors(true);
+    try {
+        $xmlTree = new SimpleXMLElement($raw_XML);
+    } catch (Exception $e) {
+        // Something went wrong.
+        $error_message = 'SimpleXMLElement threw an exception.';
+        foreach(libxml_get_errors() as $error_line) {
+            $error_message .= "\t" . $error_line->message;
+        }
+        trigger_error($error_message);
+        return false;
+    }
+    return $xmlTree;
+}
 
 
 ?>
